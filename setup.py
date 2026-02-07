6d095154d52c769d24dc62d0b3d18f6ddd7048a7
Rosy <rosypaul375@gmail.com>
2026-02-07 22:34:36 +0530

    basic requirements and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical Chat bot",
+    version="0.1.0",
+    author="Rosy Paul",
+    author_email="rosypaul475@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
